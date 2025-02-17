c4c97740253cd5a8651430fefda5930061045b75
James Bane <jamesbane0421@gmail.com>
2025-02-17 10:22:16 -0500

    add netlify toml

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 
 export default defineConfig({
   server: {
     host: '0.0.0.0',  // Allows connections from all networks
     port: 5173,       // Change if needed
   },
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
   ssr: {
     noExternal: ['@mui/icons-material'],
   },
